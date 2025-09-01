ad40952fbaf67f8cd5af1a375ae6638e1447e818
14u74r0 <lautaronicolasmontivero@gmail.com>
2025-09-01 10:51:26 -0300

    PaginaCafe

---
+++ src/button.js
@@ -0,0 +1,5 @@
+const encontranosBtn = document.getElementById('encontranosBtn');
+
+encontranosBtn.addEventListener('click', function() {
+    window.location.href = 'https://tiendadecafe.com.ar/locales';
+});
